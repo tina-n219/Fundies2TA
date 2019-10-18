3943eae3cdf97ffa0e14235b3e4759b90db9ad19
Tina <tina.noorani219@gmail.com>
2019-10-18 16:40:31 -0400

    Update JSON.java

--- JSON.java
+++ JSON.java
@@ -8,13 +8,7 @@ interface IFunc<A, R> {
 // Our IFunc<JSON, T> takes in a JSON as an input, it returns a value of type T
 interface JSONVisitor<T> extends IFunc<JSON, T> {
 
-	T visitJSONBlank(JSONBlank b);
-
 	T visitJSONNumber(JSONNumber n);
-
-	T visitJSONBool(JSONBool b);
-
-	T visitJSONString(JSONString s);
 }
 
 // Let's convert out JSON to a Number
@@ -31,31 +25,8 @@ class JSONToNumber implements JSONVisitor<Integer> {
 
 	// STEP 3: implement visitCLASS_NAME() method to perform computations with data from 
 	// parameter passed, this is where the bulk of your code should be and main logic
-	public Integer visitJSONBlank(JSONBlank b) {
-		return 0;
-	}
-
-	// STEP 3: implement visitCLASS_NAME() method to perform computations with data from 
-	// parameter passed, this is where the bulk of your code should be and main logic
 	public Integer visitJSONNumber(JSONNumber n) {
 		return n.number;
-	}
-
-	// STEP 3: implement visitCLASS_NAME() method to perform computations with data from 
-	// parameter passed, this is where the bulk of your code should be and main logic
-	public Integer visitJSONBool(JSONBool b) {
-		// return b.bool ? 1 : 0;
-		if (b.bool) {
-			return 1;
-		} else {
-			return 0;
-		}
-	}
-
-	// STEP 3: implement visitCLASS_NAME() method to perform computations with data from 
-	// parameter passed, this is where the bulk of your code should be and main logic
-	public Integer visitJSONString(JSONString s) {
-		return s.str.length();
 	}
 
 }
@@ -65,19 +36,6 @@ interface JSON {
 	// Our class has no context of generics, thus we must define our generic with `<R>` and then actually specify our 
 	// generic return type with R.
 	<R> R accept(JSONVisitor<R> visitor);
-}
-
-// no value
-class JSONBlank implements JSON {
-	// STEP 2: implement ACCEPT(Visitor<R>)
-	// Now that we are in a specific class, let's delegate to our visit specific method and 
-	// pass in our class object so that our visit method has data to work with
-	// (Note: consider why we couldn't implement the functionality of our visitClassName() method
-	// inside of our apply() method)
-	@Override
-	public <R> R accept(JSONVisitor<R> visitor) {
-		return visitor.visitJSONBlank(this);
-	}
 }
 
 // a number
@@ -96,43 +54,5 @@ class JSONNumber implements JSON {
 	@Override
 	public <R> R accept(JSONVisitor<R> visitor) {
 		return visitor.visitJSONNumber(this);
-	}
-}
-
-// a boolean
-class JSONBool implements JSON {
-	boolean bool;
-
-	JSONBool(boolean bool) {
-		this.bool = bool;
-	}
-
-	// STEP 2: implement ACCEPT(Visitor<R>)
-	// Now that we are in a specific class, let's delegate to our visit specific method and 
-	// pass in our class object so that our visit method has data to work with
-	// (Note: consider why we couldn't implement the functionality of our visitClassName() method
-	// inside of our apply() method)
-	@Override
-	public <R> R accept(JSONVisitor<R> visitor) {
-		return visitor.visitJSONBool(this);
-	}
-}
-
-// a string
-class JSONString implements JSON {
-	String str;
-
-	JSONString(String str) {
-		this.str = str;
-	}
-
-	// STEP 2: implement ACCEPT(Visitor<R>)
-	// Now that we are in a specific class, let's delegate to our visit specific method and 
-	// pass in our class object so that our visit method has data to work with
-	// (Note: consider why we couldn't implement the functionality of our visitClassName() method
-	// inside of our apply() method)
-	@Override
-	public <R> R accept(JSONVisitor<R> visitor) {
-		return visitor.visitJSONString(this);
 	}
 }
